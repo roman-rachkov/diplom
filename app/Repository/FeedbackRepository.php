2a0d92f1ff1ebd91ac8418cc2eca615cbbee7e29
Kirill Kalashnikov <kirill310587@mail.ru>
2021-08-14 01:49:30 +0700

    Complete talble with services and repositories in readme.md

--- app/Repository/FeedbackRepository.php
+++ app/Repository/FeedbackRepository.php
@@ -6,15 +6,19 @@ use App\Contracts\Repository\FeedbackRepositoryContract;
 
 class FeedbackRepository implements FeedbackRepositoryContract
 {
-    
+
     public function getAdminFeedbacks()
     {
-    
+
     }
-    
+
     public function update()
     {
-    
+
     }
-    
+
+    public function store()
+    {
+
+    }
 }
