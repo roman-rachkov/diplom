533c0220b1db8efd5be7ee8726f1d422ce3aea5c
Oleg Goloborodko <goloborodko@inbox.ru>
2021-08-24 08:45:30 +0300

    change assets links

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,19 +1,19 @@
 <footer class="Footer">
     <div class="wrap">
         <div class="row Footer-main">
-            <div class="row-block"><a class="logo Footer-logo" href="index.html"><img class="logo-image" src="assets/img/logo_footer.png" alt="logo_footer.png"/></a>
+            <div class="row-block"><a class="logo Footer-logo" href="index.html"><img class="logo-image" src="{{asset('assets/img/logo_footer.png')}}" alt="logo_footer.png"/></a>
                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincid  unt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
                 </p>
                 <ul class="menu menu_img menu_smallImg Footer-menuSoc">
-                    <li class="menu-item"><a class="menu-link" href="#"><img src="assets/img/icons/socialFooter/fb.svg" alt="fb.svg"/></a>
+                    <li class="menu-item"><a class="menu-link" href="#"><img src="{{asset('assets/img/icons/socialFooter/fb.svg')}}" alt="fb.svg"/></a>
                     </li>
-                    <li class="menu-item"><a class="menu-link" href="#"><img src="assets/img/icons/socialFooter/tw.svg" alt="tw.svg"/></a>
+                    <li class="menu-item"><a class="menu-link" href="#"><img src="{{asset('assets/img/icons/socialFooter/tw.svg')}}" alt="tw.svg"/></a>
                     </li>
-                    <li class="menu-item"><a class="menu-link" href="#"><img src="assets/img/icons/socialFooter/in.svg" alt="in.svg"/></a>
+                    <li class="menu-item"><a class="menu-link" href="#"><img src="{{asset('assets/img/icons/socialFooter/in.svg')}}" alt="in.svg"/></a>
                     </li>
-                    <li class="menu-item"><a class="menu-link" href="#"><img src="assets/img/icons/socialFooter/pt.svg" alt="pt.svg"/></a>
+                    <li class="menu-item"><a class="menu-link" href="#"><img src="{{asset('assets/img/icons/socialFooter/pt.svg')}}" alt="pt.svg"/></a>
                     </li>
-                    <li class="menu-item"><a class="menu-link" href="#"><img src="assets/img/icons/socialFooter/mail.svg" alt="mail.svg"/></a>
+                    <li class="menu-item"><a class="menu-link" href="#"><img src="{{asset('assets/img/icons/socialFooter/mail.svg')}}" alt="mail.svg"/></a>
                     </li>
                 </ul>
             </div>
@@ -68,23 +68,23 @@
                 </div>
                 <div class="row-block"><span>Accepted Payments</span>
                     <div class="Footer-payments">
-                        <div><img src="assets/img/payments/visa.png" alt="visa.png"/>
+                        <div><img src="{{asset('assets/img/payments/visa.png')}}" alt="visa.png"/>
                         </div>
-                        <div><img src="assets/img/payments/mastercard.png" alt="mastercard.png"/>
+                        <div><img src="{{asset('assets/img/payments/mastercard.png')}}" alt="mastercard.png"/>
                         </div>
-                        <div><img src="assets/img/payments/paypal.png" alt="paypal.png"/>
+                        <div><img src="{{asset('assets/img/payments/paypal.png')}}" alt="paypal.png"/>
                         </div>
-                        <div><img src="assets/img/payments/american.png" alt="american.png"/>
+                        <div><img src="{{asset('assets/img/payments/american.png')}}" alt="american.png"/>
                         </div>
-                        <div><img src="assets/img/payments/electron.png" alt="electron.png"/>
+                        <div><img src="{{asset('assets/img/payments/electron.png')}}" alt="electron.png"/>
                         </div>
-                        <div><img src="assets/img/payments/maestro.png" alt="maestro.png"/>
+                        <div><img src="{{asset('assets/img/payments/maestro.png')}}" alt="maestro.png"/>
                         </div>
-                        <div><img src="assets/img/payments/delta.png" alt="delta.png"/>
+                        <div><img src="{{asset('assets/img/payments/delta.png')}}" alt="delta.png"/>
                         </div>
-                        <div><img src="assets/img/payments/e.png" alt="e.png"/>
+                        <div><img src="{{asset('assets/img/payments/e.png')}}" alt="e.png"/>
                         </div>
-                        <div><img src="assets/img/payments/dk.png" alt="dk.png"/>
+                        <div><img src="{{asset('assets/img/payments/dk.png')}}" alt="dk.png"/>
                         </div>
                     </div>
                 </div>
