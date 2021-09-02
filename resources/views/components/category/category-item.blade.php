7f86b0be91431dc3b9d8b8ffe58c15092bcd98b3
Kirill Kalashnikov <kirill310587@mail.ru>
2021-09-02 15:04:14 +0700

    Fix docker-compose to DB service mysql, Fix categories view

--- resources/views/components/category/category-item.blade.php
+++ resources/views/components/category/category-item.blade.php
@@ -1,7 +1,7 @@
 <div class="CategoriesButton-link"><a href="#">
         <div class="CategoriesButton-icon"><img src="assets/img/icons/departments/1.svg" alt="1.svg"/>
         </div><span class="CategoriesButton-text">{{$category->name}}</span></a>
-        @if(!$category->isLeaf())
+        @if(!$category->isLeaf() && $category->children->isNotEmpty())
                 <a class="CategoriesButton-arrow" href="#"></a>
                 <x-category.category-submenu :categories="$category->children"/>
         @endif
