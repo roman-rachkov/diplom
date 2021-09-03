9f4b75c1290de40c8106fdd48c5460843f63dca8
Oleg Goloborodko <goloborodko@inbox.ru>
2021-09-03 11:20:07 +0300

    corrected AdminSettings options and migration

--- app/Repository/AdminSettingsRepository.php
+++ app/Repository/AdminSettingsRepository.php
@@ -3,8 +3,20 @@
 namespace App\Repository;
 
 use App\Contracts\Repository\AdminSettingsRepositoryContract;
+use App\Models\AdminSetting;
 
 class AdminSettingsRepository implements AdminSettingsRepositoryContract
 {
-    
+    protected $model;
+    public function __construct(AdminSetting $adminSetting)
+    {
+        $this->model = $adminSetting;
+    }
+
+    public function get(string $variable, $default=null)
+    {
+        $optionValue = $this->model->where('variable', $variable)->first()->value ?? $default;
+
+        return $optionValue;
+    }
 }
