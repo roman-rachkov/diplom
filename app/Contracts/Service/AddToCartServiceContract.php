0e26015b5a278451c1f557fdd6357c34719f11b3
Timur Moiseenko <angrav6@gmail.com>
2021-09-05 10:42:02 +0500

    methods for task 2.13 were created, the clear method was added for complete cleaning. simple logic is still implemented in methods.

--- app/Contracts/Service/AddToCartServiceContract.php
+++ app/Contracts/Service/AddToCartServiceContract.php
@@ -6,5 +6,9 @@ use App\Models\Product;
 
 interface AddToCartServiceContract
 {
-    public function add(Product $product);
+    public function add(Product $product, int $qty);
+    public function remove(int $prodId);
+    public function changeProductQuantity(int $prodId, $newQty = 1);
+    public function getProductsList();
+    public function getProductsQuantity();
 }
--- app/Service/AddToCartService.php
+++ app/Service/AddToCartService.php
@@ -7,9 +7,44 @@ use App\Models\Product;
 
 class AddToCartService implements AddToCartServiceContract
 {
+    public $cart = [];
 
-    public function add(Product $product)
+    public function add(Product $product, int $qty)
     {
-        // TODO: Implement add() method.
+
+        $this->cart[$product->id] = ['product' => $product, 'quantity' => $qty];
+        return $this->getProductsQuantity();
+    }
+
+    public function remove(int $prodId)
+    {
+        unset($this->cart[$prodId]);
+        return $this->getProductsQuantity();
+    }
+
+    public function changeProductQuantity(int $prodId, $newQty = 1)
+    {
+        $this->cart[$prodId]['quantity'] = $newQty;
+        return $this->getProductsQuantity();
+    }
+
+    public function getProductsList()
+    {
+        return $this->cart;
+    }
+
+    public function getProductsQuantity()
+    {
+        if ($this->cart) {
+            return array_sum(array_map(function ($value) {
+                return $value['quantity'];
+            }, $this->cart));
+        }
+        return 0;
+    }
+
+    public function clear() {
+        $this->cart = [];
+        return $this->getProductsQuantity();
     }
 }
