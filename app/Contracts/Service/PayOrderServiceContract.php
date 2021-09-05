1b8fda390e774cb24e2630702a8258d6ddbf8082
Timur Moiseenko <angrav6@gmail.com>
2021-09-05 12:41:53 +0500

    methods for task 2.16 were created

--- app/Contracts/Service/PayOrderServiceContract.php
+++ app/Contracts/Service/PayOrderServiceContract.php
@@ -5,4 +5,6 @@ namespace App\Contracts\Service;
 interface PayOrderServiceContract
 {
     public function pay($payMethod, $cardNumber);
+
+    public function getStatus($paymentId);
 }
--- app/Service/PayOrderService.php
+++ app/Service/PayOrderService.php
@@ -6,9 +6,23 @@ use App\Contracts\Service\PayOrderServiceContract;
 
 class PayOrderService implements PayOrderServiceContract
 {
+    public $payments = [];
 
     public function pay($payMethod, $cardNumber)
     {
-        // TODO: Implement pay() method.
+        // send in payment queue -> job call PaymentMethod and create DB new rows id / method_name / status / error_message
+        $id = count($this->payments) + 1;
+        $error = rand(0, 1);
+        $this->payments[$id] = [
+            'method' => $payMethod,
+            'status' => $error,
+            'message' => $error ? "" : "Error payment status"
+             ];
+        return $this->getStatus($id);
+    }
+
+    public function getStatus($id)
+    {
+        return $this->payments[$id]['status'];
     }
 }
