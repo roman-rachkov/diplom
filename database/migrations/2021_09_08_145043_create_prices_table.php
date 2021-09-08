3b728e08fe70ae00132fd094c63b559f574886b9
Kirill Kalashnikov <kirill310587@mail.ru>
2021-09-08 22:43:41 +0700

    Added Price model, migration, factory, relation with Product

---
+++ database/migrations/2021_09_08_145043_create_prices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seller_id');
+            $table->unsignedBigInteger('product_id');
+            $table->timestamps();
+
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+release=$1
+task=$2
+
+git branch "$release"
+git pull "$release"
+git branch "$task"
+git checkout "$task"
+bash ./vendor/bin/sail up
+bash ./vendor/bin/sail composer install
+
+
+#echo `git branch $1`
+#echo `git pull $1`
+#echo `git branch $2`
+#echo `git checkout $`
+
+#bash git $1
+#bash ./vendor/bin/sail composer install
+#bash ./vendor/bin/sail artisan migrate:fresh
+#bash ./vendor/bin/sail npm install
+#bash ./vendor/bin/sail npm run dev
+#bash ./vendor/bin/sail artisan db:seed \\Database\\Seeders\\DemoDataSeeders\\DemoDataSeeder
