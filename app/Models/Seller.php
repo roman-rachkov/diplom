0072bc4a772192f08ca9851ed730b2fbebc9418f
Oleg Goloborodko <goloborodko@inbox.ru>
2021-09-08 15:15:41 +0300

    add migration for sellers

---
+++ app/Models/Seller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Orchid\Attachment\Attachable;
+
+class Seller extends Model
+{
+    use HasFactory;
+    use Attachable;
+
+}
