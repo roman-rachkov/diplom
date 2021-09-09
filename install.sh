71fe4c3cb07e4256c138bc03c9722086b4bd502a
Kirill Kalashnikov <kirill310587@mail.ru>
2021-09-09 20:34:04 +0700

    Modify install.sh composer install, npu install, npm run dev

--- install.sh
+++ install.sh
@@ -5,20 +5,8 @@ task=$2
 
 git branch "$release"
 git pull origin "$release"
-git branch "$task"
-git checkout "$task"
-bash ./vendor/bin/sail up
+git checkout -b "$task"
+bash ./vendor/bin/sail up &
 bash ./vendor/bin/sail composer install
-
-
-#echo `git branch $1`
-#echo `git pull $1`
-#echo `git branch $2`
-#echo `git checkout $`
-
-#bash git $1
-#bash ./vendor/bin/sail composer install
-#bash ./vendor/bin/sail artisan migrate:fresh
-#bash ./vendor/bin/sail npm install
-#bash ./vendor/bin/sail npm run dev
-#bash ./vendor/bin/sail artisan db:seed \\Database\\Seeders\\DemoDataSeeders\\DemoDataSeeder
+bash ./vendor/bin/sail npm install
+bash ./vendor/bin/sail npm run dev
