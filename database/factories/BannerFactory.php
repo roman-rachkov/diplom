fd01b8aaa8daec5bc13365f7125fb3f670cbda6a
Timur Moiseenko <angrav6@gmail.com>
2021-09-12 07:36:00 +0500

    remove excess code

--- database/factories/BannerFactory.php
+++ database/factories/BannerFactory.php
@@ -4,9 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Banner;
 use App\Models\Model;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Orchid\Attachment\Models\Attachment;
 
 class BannerFactory extends Factory
@@ -20,30 +18,6 @@ class BannerFactory extends Factory
 
     public function definition()
     {
-        $datePath = \Illuminate\Support\Carbon::now()->format('Y/m/d');
-        $dir = storage_path('app/public/' . $datePath) ;
-        if(!is_dir($dir)) {
-            mkdir($dir, 0777, true);
-        }
-        $image = $this->faker->image($dir,640,480, null, false);
-        list($name, $extension) = explode('.', $image);
-
-//        $image_id = Attachment::create([
-//            'name' => $name,
-//            'original_name' => $image,
-//            'mime' => mime_content_type($dir . '/' . $image),
-//            'extension' => $extension,
-//            'size' => stat($dir . '/' . $image)['size'],
-//            'sort' => 0,
-//            'path' => $datePath,
-//            'description' => 'Add from BannerAttachmentSeeder',
-//            'alt' => $image,
-//            'hash' => Hash::make($name),
-//            'disk' => 'public',
-//            'user_id' => User::all()->random()->id,
-//            'group' => 0
-//        ]);
-
         return [
             'title' => $this->faker->sentence(3),
             'subtitle' => $this->faker->sentence(15),
