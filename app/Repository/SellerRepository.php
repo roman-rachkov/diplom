b87cc089e972d2513200cdb4e0c2d0db8f21c670
Oleg Goloborodko <goloborodko@inbox.ru>
2021-09-15 21:54:51 +0300

    Tasck 2.17 Правка кэша

--- app/Repository/SellerRepository.php
+++ app/Repository/SellerRepository.php
@@ -2,21 +2,27 @@
 
 namespace App\Repository;
 
+use App\Contracts\Repository\AdminSettingsRepositoryContract;
 use App\Contracts\Repository\SellerRepositoryContract;
 use App\Models\Seller;
+use Illuminate\Support\Facades\Cache;
 
 class SellerRepository implements SellerRepositoryContract
 {
-    public $model;
+    private $model;
+    private $adminsSettings;
 
-    public function __construct(Seller $seller)
+    public function __construct(Seller $seller, AdminSettingsRepositoryContract $adminsSettings)
     {
         $this->model = $seller;
+        $this->adminsSettings = $adminsSettings;
     }
 
     public function find($id)
     {
-        $seller = cache()->tags(['sellers'])->remember('seller' . $id, 600, function () use ($id) {
+        $ttl = $this->adminsSettings->get('bannerCacheTime', 600);
+
+        $seller = Cache::tags(['sellers'])->remember('seller' . $id, $ttl, function () use ($id) {
 
             return $this->model->find($id);
         });
