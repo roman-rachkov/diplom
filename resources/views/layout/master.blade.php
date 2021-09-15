6b79c968b400f7cdeee5469f0e0b87b028ee2751
Kirill Kalashnikov <kirill310587@mail.ru>
2021-09-15 21:44:04 +0700

    Added css styles to alert component to basic.css, removed bootstrap stylesheet from master

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -29,8 +29,6 @@
     <link rel="preload" href="{{asset('fonts/Roboto-Light.woff')}}" as="font">
     <link rel="preload" href="{{asset('fonts/Roboto-Light_Italic.woff')}}" as="font">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 </head>
 <body class="Site">
 <!--if lt IE 8
