35d4c5f65db5cdc293c1f5fb379df91c3878e7d9
Oleg Goloborodko <goloborodko@inbox.ru>
2021-09-29 09:10:19 +0300

    Задание 26 Добавление tagsArr в модель User

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,8 @@ class User extends Authenticatable
 {
     use Attachable, FlushTagCache;
 
+    public static $tagsArr = ['users'];
+
     /**
      * The attributes that are mass assignable.
      *
