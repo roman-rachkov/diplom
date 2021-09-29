2b921046aec5ae9f8df2c4dcc1e9910c8301eedd
Kirill Kalashnikov <kirill310587@mail.ru>
2021-09-29 20:40:14 +0700

    Fixed js/ajax.js to hide show_more button after getting last reviews page

--- resources/js/ajax.js
+++ resources/js/ajax.js
@@ -12,6 +12,10 @@
           var nextPage = $(this).data('page') + 1;
 
           axios.post(route, {'current_page': nextPage}).then(res => {
+            if (res.data.last_page == res.data.current_page) {
+              $addReviewsButton.hide();
+              return;
+            }
             $(this).data('page', res.data.current_page);
             res.data.data.forEach(comment => {
               let $commentClone = $('.Comment').first().clone();
