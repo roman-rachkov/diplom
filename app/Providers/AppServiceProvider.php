8fb9a3259fec2c1bc442a4928d9c3576df2a66d3
Rachkov Roman <pakycb84@gmail.com>
2021-10-05 16:30:29 +1000

    fixed same hash on new customer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -71,6 +71,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(Customer::class, function () {
             $customer = Customer::firstOrNew(['hash' => Cookie::get('customer_token')]);;
             if ($customer->hash === null) {
+                $customer->save();
                 $customer->hash = hash('sha256', $customer);
                 Cookie::queue(Cookie::forever('customer_token', $customer->hash));
             }
