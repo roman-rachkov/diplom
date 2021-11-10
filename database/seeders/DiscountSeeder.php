f462cf259c953382a268bb9a2081e9ac4a407fb0
Rachkov Roman <pakycb84@gmail.com>
2021-11-10 14:15:07 +1000

    rewrite seeders

--- database/seeders/DiscountSeeder.php
+++ database/seeders/DiscountSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Discount;
 use App\Models\DiscountGroup;
-use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DiscountSeeder extends Seeder
@@ -17,13 +16,25 @@ class DiscountSeeder extends Seeder
     public function run()
     {
         Discount::factory([
-            'method_type' => Discount::METHOD_CLASSIC,
-        ])->count(10)->create();
+            'category_type' => Discount::CATEGORY_OTHER,
+        ])->has(
+            DiscountGroup::factory()
+                ->count(1)
+                ->hasProducts(random_int(0, 10))
+                ->hasCategories(random_int(0, 10))
+        )->count(10)->create();
+
         Discount::factory([
-            'method_type' => Discount::METHOD_FIXED,
-        ])->count(10)->create();
+            'category_type' => Discount::CATEGORY_SET,
+        ])->has(
+            DiscountGroup::factory()
+                ->count(random_int(1, 5))
+                ->hasProducts(random_int(0, 10))
+                ->hasCategories(random_int(0, 10))
+        )->count(10)->create();
+
         Discount::factory([
-            'method_type' => Discount::METHOD_SUM,
+            'category_type' => Discount::CATEGORY_CART,
         ])->count(10)->create();
     }
 }
