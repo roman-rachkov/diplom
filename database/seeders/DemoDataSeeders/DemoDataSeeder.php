57337b740747c67139840e5b76f68e1e67939f02
Mikhail Volkov <m.volkov@qsoft.ru>
2021-11-16 17:15:31 +0300

    Заготовка для создания собственных сидеров с демоданными

---
+++ database/seeders/DemoDataSeeders/DemoDataSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders\DemoDataSeeders;
+
+use Illuminate\Database\Seeder;
+
+class DemoDataSeeder extends Seeder
+{
+    /**
+     * command to run it:
+     * sail artisan db:seed \\Database\\Seeders\\DemoDataSeeders\\DemoDataSeeder
+     */
+    public function run()
+    {
+        $this->call(
+            [
+                TmoiseenkoSeeder::class,
+                TftpOSSeeder::class,
+                Pakycb84Seeder::class,
+                SkydescentSeeder::class,
+            ]
+        );
+    }
+}
