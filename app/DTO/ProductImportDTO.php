ea6bc84e7a4f8a04b7d45cb506fdf0769190359e
Rachkov Roman <pakycb84@gmail.com>
2021-11-26 14:03:11 +1000

    refactoring

--- app/DTO/ProductImportDTO.php
+++ app/DTO/ProductImportDTO.php
@@ -7,11 +7,12 @@ class ProductImportDTO implements DataTransferObjectInterface
     public string $name;
     public string $slug;
     public string $description;
-    public string $fullDescription;
-    public int $category;
-    public int $manufacturer;
-    public int $seller;
+    public string $full_description;
+    public int $category_id;
+    public int $manufacturer_id;
+    public int $seller_id;
     public float $price;
+    public bool $limited_edition;
 
     public static function create(mixed $args): DataTransferObjectInterface
     {
@@ -22,20 +23,22 @@ class ProductImportDTO implements DataTransferObjectInterface
         string $name,
         string $slug,
         string $description,
-        string $fullDescription,
-        int $category,
-        int $manufacturer,
-        int $seller,
-        float $price,
+        string $full_description,
+        bool   $limited_edition,
+        int    $category_id,
+        int    $manufacturer_id,
+        int    $seller_id,
+        float  $price,
     )
     {
         $this->name = $name;
         $this->slug = $slug;
         $this->description = $description;
-        $this->fullDescription = $fullDescription;
-        $this->category = $category;
-        $this->manufacturer = $manufacturer;
-        $this->seller = $seller;
+        $this->full_description = $full_description;
+        $this->category_id = $category_id;
+        $this->manufacturer_id = $manufacturer_id;
+        $this->seller_id = $seller_id;
         $this->price = $price;
+        $this->limited_edition = $limited_edition;
     }
 }
