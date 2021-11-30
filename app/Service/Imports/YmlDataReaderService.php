482223d7412c9c787cd855f8fd0e64dffdc4982a
Kirill Kalashnikov <kirill310587@mail.ru>
2021-11-30 12:08:47 +0700

    Fix Yml

--- app/Service/Imports/YmlDataReaderService.php
+++ app/Service/Imports/YmlDataReaderService.php
@@ -17,41 +17,9 @@ class YmlDataReaderService implements DataReaderContract
 
     public function getData(): Collection
     {
-        $yml = new YML();
+        $yml = simplexml_load_file($this->file->getRealPath());
 
-        try {
-            $yml->parse($this->file->getRealPath());
-            $date = $yml->getDate();
-            $shop = $yml->getShop();
-            if ($shop->isValid()) {
-                $offersCount = $shop->getOffersCount();
-                $shopData = $shop->getData();
-                //...
-//                foreach ($yml->getOffers() as $offer) {
-//                    if ($offer->isValid()) {
-//                        $offerCategoryHierarchy = $shop->getCategoryHierarchy($offer->getCategoryId());
-//                        $offerData = $offer->getData();
-//                        //...
-//                    } else {
-//                        var_dump($offer->getErrors());
-//                        //...
-//                    }
-//                }
-            } else {
-                var_dump($shop->getErrors());
-                //...
-            }
-        } catch (\Exception $e) {
-            echo $e->getMessage();
-            //...
-        }
 
-        return collect(
-            [
-                'date' => $date,
-                'shop' => $shop,
-                'offers' => $offersCount,
-                'shop_data' => $shopData,
-            ]);
+
     }
 }
