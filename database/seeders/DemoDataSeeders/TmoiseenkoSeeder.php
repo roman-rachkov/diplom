f5971b7c3a39cbc464ba941c3edc0366ecf0e1c8
Timur Moiseenko <angrav6@gmail.com>
2021-12-07 16:04:44 +0500

    сидер готов.

--- database/seeders/DemoDataSeeders/TmoiseenkoSeeder.php
+++ database/seeders/DemoDataSeeders/TmoiseenkoSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders\DemoDataSeeders;
 use App\Models\Banner;
 use App\Models\Category;
 use App\Models\Manufacturer;
+use App\Models\Price;
+use App\Models\Product;
+use App\Models\Seller;
 use App\Models\User;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Database\Seeder;
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Orchid\Attachment\Models\Attachment;
+use Orchid\Attachment\Models\Attachmentable;
 
 /**
  * run: sail artisan db:seed --class=Database\\Seeders\\DemoDataSeeders\\TmoiseenkoSeeder
@@ -21,6 +25,59 @@ class TmoiseenkoSeeder extends Seeder
 {
     public function run()
     {
+        $images = [
+            'new_year' => [
+                'product-1-2.jpg',
+                'product-1-3.jpg',
+                'product-1-4.jpg',
+                'product-1-5.jpg',
+            ],
+            'valentines' => [
+                'valentine-2.jpg',
+                'valentine-3.jpg',
+                'valentine-4.jpg',
+            ],
+        ];
+
+        $prices = collect([
+            5482.15,
+            1658.12,
+            4500.99,
+            4659.00,
+            8768.25,
+            10548.55,
+            3598.14,
+        ]);
+
+        $sellersArr = [
+            [
+                'name' => 'Продавец №1',
+                'description' => 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.',
+                'email' => 'seller-1@sellers.ee',
+                'phone' => '1234567890',
+                'address' => '431568, г. Ишеевка, ул. Академика Несмеянова',
+                'logo_id' => Attachment::factory($this->prepareAttachment('manufactureres.jpg'))->create(),
+            ],
+            [
+                'name' => 'Продавец №2',
+                'description' => 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.',
+                'email' => 'seller-2@sellers.ee',
+                'phone' => '1234567890',
+                'address' => '659084, г. Вишневое, ул. Старонародная',
+                'logo_id' => Attachment::factory($this->prepareAttachment('manufactureres.jpg'))->create(),
+            ],
+            [
+                'name' => 'Продавец №3',
+                'description' => 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.',
+                'email' => 'seller-3@sellers.ee',
+                'phone' => '1234567890',
+                'address' => '369160, г. Подольск, ул. Хвойная, дом 25,',
+                'logo_id' => Attachment::factory($this->prepareAttachment('manufactureres.jpg'))->create(),
+            ],
+        ];
+
+        $sellersObj = [];
+
         User::factory([
             'name' => 'tmoiseenko',
             'email' => 'tmoiseenko@admin.com',
@@ -67,7 +124,6 @@ class TmoiseenkoSeeder extends Seeder
             'is_active' => true,
         ])->create();
 
-
         $valentinesYearCat =  Category::factory([
             'name' => 'Подарочние наборы',
             'slug' => "valentines_day",
@@ -76,11 +132,71 @@ class TmoiseenkoSeeder extends Seeder
             'is_active' => true,
         ])->create();
 
+        $newProduct =  Product::factory([
+            'name' => 'Новогодний набор',
+            'description' => 'Набор от голландского бренда House of Seasons состоит из 26 стеклянных шаров разного диаметра ( O 5СМ - 8 шт; O 6СМ - 6 шт; O 7СМ - 12шт) в ярко- розовом цвете',
+            'full_description' => 'Набор от голландского бренда House of Seasons состоит из 26 стеклянных шаров разного диаметра ( O 5СМ - 8 шт; O 6СМ - 6 шт; O 7СМ - 12шт) в ярко- розовом цвете, упакован в стильную подарочную коробку. Благодаря сочетанию матовых и глянцевых фактур шариков в данной коллекции, а также их яркому и насыщенному исполнению, Вы невероятно легко и с удовольствием создадите неповторимый дизайн для Вашей елочки!',
+            'slug' => 'new_year_product',
+            'sort_index' => 0,
+            'sales_count' => 0,
+            'limited_edition' => true,
+            'manufacturer_id' => $newYearManufacturer->id,
+            'main_img_id' => Attachment::factory($this->prepareAttachment('products/product-1-1.jpg')),
+            'category_id' => $newYearCat->id,
+        ])->create();
+
+        $valentinesProduct =  Product::factory([
+            'name' => 'Сладкий набор ко дню влюбленных',
+            'description' => 'Набор от голландского бренда House of Seasons состоит из 26 стеклянных шаров разного диаметра ( O 5СМ - 8 шт; O 6СМ - 6 шт; O 7СМ - 12шт) в ярко- розовом цвете',
+            'full_description' => 'Набор от голландского бренда House of Seasons состоит из 26 стеклянных шаров разного диаметра ( O 5СМ - 8 шт; O 6СМ - 6 шт; O 7СМ - 12шт) в ярко- розовом цвете, упакован в стильную подарочную коробку. Благодаря сочетанию матовых и глянцевых фактур шариков в данной коллекции, а также их яркому и насыщенному исполнению, Вы невероятно легко и с удовольствием создадите неповторимый дизайн для Вашей елочки!',
+            'slug' => 'valentines_product',
+            'sort_index' => 0,
+            'sales_count' => 0,
+            'limited_edition' => true,
+            'manufacturer_id' => $valentinesManufacturer->id,
+            'main_img_id' => Attachment::factory($this->prepareAttachment('products/valentine-1.jpg')),
+            'category_id' => $valentinesYearCat->id,
+        ])->create();
+
+        foreach ($sellersArr as $seller) {
+            $sellersObj[] = Seller::factory()->create($seller);
+        }
+
+        foreach ($images['new_year'] as $image) {
+            $newProduct->additionalImages()->saveMany(new Attachmentable([
+                'attachmentable_type' => Attachment::class,
+                'attachmentable_id' => $newProduct->id,
+                'attachment_id' => Attachment::factory($this->prepareAttachment('products/' . $image)),
+            ]));
+        }
+
+        foreach ($images['valentines'] as $image) {
+            $valentinesProduct->additionalImages()->saveMany(new Attachmentable([
+                'attachmentable_type' => Attachment::class,
+                'attachmentable_id' => $valentinesProduct->id,
+                'attachment_id' => Attachment::factory($this->prepareAttachment('products/' . $image)),
+            ]));
+        }
+
+        foreach ($sellersObj as $seller) {
+            Price::factory()->create([
+                'price' => $prices->random(),
+                'product_id' => $newProduct->id,
+                'seller_id' => $seller->id,
+            ]);
+
+            Price::factory()->create([
+                'price' => $prices->random(),
+                'product_id' => $valentinesProduct->id,
+                'seller_id' => $seller->id,
+            ]);
+        }
+
         Banner::factory([
             'title' => 'Новогодиние скидки',
             'subtitle' => 'В нашем магазине действую новогодние скидки на все товары в размере 30%',
             'button_text' => 'Узнать больше',
-            'href' => 'https://laraveldiplomagroup01.mvsvolkov.ru/discounts',
+            'href' => 'https://laraveldiplomagroup01.mvsvolkov.ru/products/new_year_product',
             'is_active' => true,
             'image_id' => Attachment::factory($this->prepareAttachment('banners/banner-1.jpg')),
         ])->create();
@@ -98,7 +214,7 @@ class TmoiseenkoSeeder extends Seeder
             'title' => 'Специальное предложение ко Дню Святого Валентина',
             'subtitle' => 'Уникальное предложение купить подарочный набор своей любимой уже сейчас, по специальной цене.',
             'button_text' => 'Узнать больше',
-            'href' => 'https://laraveldiplomagroup01.mvsvolkov.ru/discounts',
+            'href' => 'https://laraveldiplomagroup01.mvsvolkov.ru/products/valentines_product',
             'is_active' => true,
             'image_id' => Attachment::factory($this->prepareAttachment('banners/banner-1.jpg')),
         ])->create();
