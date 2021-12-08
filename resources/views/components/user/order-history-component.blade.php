1a89053a4bf6c395539338b8501a222c73b16464
Kirill Kalashnikov <kirill310587@mail.ru>
2021-12-08 19:52:09 +0700

    Fixed order-history-component

--- resources/views/components/user/order-history-component.blade.php
+++ resources/views/components/user/order-history-component.blade.php
@@ -25,7 +25,11 @@
                     {{$order->payment->paymentsService->name ?? ''}}
                 </x-info-component>
                 <x-info-component :title="__('profile.orders.totalCost')">
-                    <span class="Order-price">{{$order->total}}$</span>
+                    @if($order->payment?->payed_at != null)
+                        <span class="Order-price">{{$order->total}}$</span>
+                    @else
+                        <span class="Order-price">{{$orderService->getTotalCost()}}$</span>
+                    @endif
                 </x-info-component>
                 <x-info-component :title="__('profile.orders.status')" classes="Order-info_status">
                     @if($order->payment?->payed_at != null)
